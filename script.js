a77cf7d58b5379727d82417f56b65939bd92ab05
davi0801 <113618958+davi0801@users.noreply.github.com>
2024-07-01 11:02:33 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPeruntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".textoresultado");
+
+const caixaResultado1 = document.querySelector(".caixa-principal");
+
+const perguntas = [
+    {
+enunciado:"O futebol tem prejudicado muitos lugares devido o vandalismo e a poluição, isso pode influenciar as pessoas fazerem a mesma coisa em outros esportes. Para que isso não aconteça o que deve ser feito ?",
+alternativas: [
+    "Ter mais polícias presente nos eventos."
+    "para a segurança a preservação do lugar."
+]
+    }
+]
